0d6ada5497373cf80b0b8d7e9a10de53af3bb1d8
Grant Baum <grantinator@gmail.com>
2024-11-02 19:11:36 -0700

    Create basic_query.py manually (my git auth is messed up)
    In order to work you need to have ollama running in a terminal window with `ollama serve`

---
+++ grant/basic_query.py
@@ -0,0 +1,34 @@
+import ollama
+import requests
+
+def call_and_stream_results(query):
+    r = requests.post('http://localhost:11434/api/generate',
+        json={
+            'model': 'llama3',
+            'prompt': prompt,
+            'context': [],
+        },
+        stream=True)
+    r.raise_for_status()
+    
+    full_response = ""
+    for l in r.iter_lines():
+        response = json.loads(l)
+
+        full_response += response.get('response', '')
+
+    print(full_response)
+
+
+def main():
+    # context = [] # the context stores a conversation history, you can use this to make the model more context aware
+    while True:
+        user_input = input("Enter a prompt: ")
+        if not user_input:
+            exit()
+        print()
+        call_and_stream_results(user_input)
+        print()
+
+if __name__ == "__main__":
+    main()
